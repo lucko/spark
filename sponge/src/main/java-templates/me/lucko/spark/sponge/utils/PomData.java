7d6808cbcfbb0f61f93e536d36968eeda5bd302c
Luck <git@lucko.me>
2018-06-06 15:39:16 +0100

    Convert to Gradle

--- sponge/src/main/java-templates/me/lucko/spark/sponge/utils/PomData.java
+++
@@ -1,8 +0,0 @@
-package me.lucko.spark.sponge.utils;
-
-public class PomData {
-
-    public static final String DESCRIPTION = "${project.description}";
-    public static final String VERSION = "${project.version}";
-
-}
