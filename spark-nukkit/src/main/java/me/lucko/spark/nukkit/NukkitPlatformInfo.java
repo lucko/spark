7802172459179ce56419c6d3d2a9d43571e5c639
Luke <32024335+lukeeey@users.noreply.github.com>
2021-02-04 18:22:51 +0000

    Add support for Cloudburst Nukkit (#95)

---
+++ spark-nukkit/src/main/java/me/lucko/spark/nukkit/NukkitPlatformInfo.java
@@ -0,0 +1,33 @@
+package me.lucko.spark.nukkit;
+
+import cn.nukkit.Server;
+import cn.nukkit.network.protocol.ProtocolInfo;
+import me.lucko.spark.common.platform.AbstractPlatformInfo;
+
+public class NukkitPlatformInfo extends AbstractPlatformInfo {
+    private final Server server;
+
+    public NukkitPlatformInfo(Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public Type getType() {
+        return Type.SERVER;
+    }
+
+    @Override
+    public String getName() {
+        return "Nukkit";
+    }
+
+    @Override
+    public String getVersion() {
+        return this.server.getNukkitVersion();
+    }
+
+    @Override
+    public String getMinecraftVersion() {
+        return this.server.getVersion();
+    }
+}
