8a9db93a892b671c47614bba4cb5ef047866377f
Bram Ceulemans <bramceulemans@me.com>
2021-08-19 18:42:37 +0200

    Add JavaDoc to Profiler API

--- spark-api/src/main/java/me/lucko/spark/api/profiler/Profiler.java
+++ spark-api/src/main/java/me/lucko/spark/api/profiler/Profiler.java
@@ -1,8 +1,23 @@
 package me.lucko.spark.api.profiler;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface Profiler {
 
+    /**
+     * Start the Spark Profiler using a profiler configuration.
+     * A configuration can be built using {@link ProfilerConfiguration#builder()}.
+     *
+     * @param configuration the configuration object
+     * @return if the profiler started successfully
+     */
     boolean start(ProfilerConfiguration configuration);
 
+    /**
+     * Stop the currently running profiler.
+     *
+     * @return the profiler report or null if no profiler was running.
+     */
+    @Nullable
     ProfilerReport stop();
 }
