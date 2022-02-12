8bfad57b70746f444c9fe74e6be3d078ae4da247
Luck <git@lucko.me>
2022-02-12 00:32:56 +0000

    Improve Sponge and Fabric class source lookups

--- spark-sponge8/src/main/java/me/lucko/spark/sponge/Sponge8ClassSourceLookup.java
+++ spark-sponge8/src/main/java/me/lucko/spark/sponge/Sponge8ClassSourceLookup.java
@@ -20,25 +20,65 @@
 
 package me.lucko.spark.sponge;
 
+import com.google.common.collect.ImmutableMap;
+
 import me.lucko.spark.common.util.ClassSourceLookup;
 
 import org.spongepowered.api.Game;
+import org.spongepowered.plugin.PluginCandidate;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.builtin.jvm.JVMPluginContainer;
+import org.spongepowered.plugin.builtin.jvm.locator.JVMPluginResource;
 
+import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Map;
 
 public class Sponge8ClassSourceLookup extends ClassSourceLookup.ByCodeSource {
     private final Path modsDirectory;
+    private final Map<Path, String> pathToPluginMap;
 
     public Sponge8ClassSourceLookup(Game game) {
         this.modsDirectory = game.gameDirectory().resolve("mods").toAbsolutePath().normalize();
+        this.pathToPluginMap = constructPathToPluginIdMap(game.pluginManager().plugins());
     }
 
     @Override
     public String identifyFile(Path path) {
+        String id = this.pathToPluginMap.get(path);
+        if (id != null) {
+            return id;
+        }
+
         if (!path.startsWith(this.modsDirectory)) {
             return null;
         }
 
         return super.identifyFileName(this.modsDirectory.relativize(path).toString());
     }
+
+    // pretty nasty, but if it fails it doesn't really matter
+    @SuppressWarnings("unchecked")
+    private static Map<Path, String> constructPathToPluginIdMap(Collection<PluginContainer> plugins) {
+        ImmutableMap.Builder<Path, String> builder = ImmutableMap.builder();
+
+        try {
+            Field candidateField = JVMPluginContainer.class.getDeclaredField("candidate");
+            candidateField.setAccessible(true);
+
+            for (PluginContainer plugin : plugins) {
+                if (plugin instanceof JVMPluginContainer) {
+                    PluginCandidate<JVMPluginResource> candidate = (PluginCandidate<JVMPluginResource>) candidateField.get(plugin);
+                    Path path = candidate.resource().path().toAbsolutePath().normalize();
+                    builder.put(path, plugin.metadata().id());
+                }
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+
+        return builder.build();
+    }
+
 }
