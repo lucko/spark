8b6c26636b19464e43f99ab17b6059de7ad436a5
Luck <git@lucko.me>
2024-05-06 17:49:35 +0100

    Update Forge to MC 1.20.6

--- spark-forge/src/main/java/me/lucko/spark/forge/ForgeCommandSender.java
+++ spark-forge/src/main/java/me/lucko/spark/forge/ForgeCommandSender.java
@@ -26,6 +26,7 @@ import me.lucko.spark.forge.plugin.ForgeSparkPlugin;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.minecraft.commands.CommandSource;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.chat.Component.Serializer;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.server.MinecraftServer;
@@ -66,7 +67,7 @@ public class ForgeCommandSender extends AbstractCommandSender<CommandSource> {
 
     @Override
     public void sendMessage(Component message) {
-        MutableComponent component = Serializer.fromJson(GsonComponentSerializer.gson().serializeToTree(message));
+        MutableComponent component = Serializer.fromJson(GsonComponentSerializer.gson().serializeToTree(message), RegistryAccess.EMPTY);
         Objects.requireNonNull(component, "component");
         super.delegate.sendSystemMessage(component);
     }
