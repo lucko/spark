a4798ce832b66ccda860f9951bef924d51f149c9
Drex <nicknamedrex@gmail.com>
2024-06-14 02:24:18 +0200

    Update to 1.21

--- spark-fabric/src/main/java/me/lucko/spark/fabric/mixin/ClientEntityManagerAccessor.java
+++ spark-fabric/src/main/java/me/lucko/spark/fabric/mixin/ClientEntityManagerAccessor.java
@@ -20,8 +20,8 @@
 
 package me.lucko.spark.fabric.mixin;
 
-import net.minecraft.client.world.ClientEntityManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.world.entity.ClientEntityManager;
 import net.minecraft.world.entity.EntityIndex;
 import net.minecraft.world.entity.SectionedEntityCache;
 
